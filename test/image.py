87bfde7edb413061f9304350262c5a05a28670ec
nashiro66 <mkuwana@keio.jp>
2025-10-16 12:33:30 +0900

    戻した

--- test/image.py
+++ test/image.py
@@ -3,16 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # --- 画像を読み込み ---
-albedo = cv2.imread("mat-kiwi.nested_bsdf.single_scattering_albedo.data_mip_00.png").astype(np.float32) / 255.0
+extinction = cv2.imread("sigma_t.png").astype(np.float32) / 255.0
+albedo = cv2.imread("albedo.png").astype(np.float32) / 255.0
+
+# --- albedoをextinctionと同じサイズにリサイズ ---
+albedo_resized = cv2.resize(albedo, (extinction.shape[1], extinction.shape[0]), interpolation=cv2.INTER_LINEAR)
 
 # --- 掛け算して 1 - result を計算 ---
-result = 1.13131 * (1.0-albedo)
-inv_result = result  # 反転画像
+result = extinction * albedo_resized
+inv_result = 1.0 - result  # 反転画像
 
 # --- 結果を保存 ---
+result_img = np.clip(result * 255, 0, 255).astype(np.uint8)
 inv_img = np.clip(inv_result * 255, 0, 255).astype(np.uint8)
 
+cv2.imwrite("output_result.png", result_img)
 cv2.imwrite("output_inverted.png", inv_img)
+
+# --- 表示 ---
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+plt.title("Result (extinction × albedo)")
+plt.imshow(cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB))
+plt.axis("off")
 
 plt.subplot(1, 2, 2)
 plt.title("1 - Result (Inverted)")

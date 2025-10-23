ec6b9ececffcbe359b09b98909f246f77a267445
nashiro66 <mkuwana@keio.jp>
2025-10-24 01:57:22 +0900

    phase functionを適用する前

--- python/core/integrators/volume_utils.py
+++ python/core/integrators/volume_utils.py
@@ -39,18 +39,6 @@ def eval_hg(g: mi.Spectrum | mi.Float, cos_theta: mi.Float):
   return dr.inv_four_pi * (1.0 - dr.square(g)) / (temp * dr.sqrt(temp))
 
 
-def sample_isotropic(sample2: mi.Float):
-  phase_wo = mi.warp.square_to_uniform_sphere(sample2)
-  pdf = mi.warp.square_to_uniform_sphere_pdf(phase_wo)
-  weight = mi.Spectrum(1.0)
-  return phase_wo, weight, pdf
-
-
-def eval_pdf_isotropic(phase_wo):
-  pdf = mi.warp.square_to_uniform_sphere_pdf(phase_wo)
-  return pdf, pdf
-
-
 def sample_spectral_hg(
     g: mi.Spectrum, ray: mi.Ray3f, sample2: mi.Vector2f, channel: mi.UInt32
 ):
@@ -83,18 +71,9 @@ def sample_spectral_hg(
   return wo, weight, eval_spectral_hg(g, wo, ray)
 
 
-def eval_pdf_hg(g, phase_wo, ray, channel):
-  g = index_spectrum(mi.Spectrum(g), channel)
-  pdf = eval_hg(g, dr.dot(phase_wo, -ray.d))
-  return pdf, pdf
-
-
 def eval_spectral_hg(g, phase_wo, ray):
   return eval_hg(g, dr.dot(phase_wo, -ray.d))
 
-
-def pdf_spectral_hg(g, phase_wo, ray):
-  return eval_hg(g, dr.dot(phase_wo, -ray.d))
 
 
 class SSSInteraction:
@@ -116,21 +95,6 @@ class SSSInteraction:
     self.sigma_t = mi.Spectrum(0.0)
     self.sigma_s = mi.Spectrum(0.0)
     self.g = mi.Spectrum(0.0)
-
-  # We don't use these (yet?) but force HG, g = 0.0 is isotropic anyway
-  # def sample_phase(self, ray, sample2):
-  #   return dr.select(
-  #       (self.g[0] != 0.0),
-  #       sample_hg(self.g, ray, sample2),
-  #       sample_isotropic(sample2),
-  #   )
-
-  # def eval_pdf_phase(self, phase_wo, ray):
-  #   return dr.select(
-  #       self.g[0] != 0.0,
-  #       eval_pdf_hg(self.g, phase_wo, ray),
-  #       eval_pdf_isotropic(phase_wo),
-  #   )
 
   def is_valid(self):
     return self.t != dr.inf
@@ -166,6 +130,7 @@ class SSSMedium:
     )
 
     self.bsdf_has_sss[active] = bsdf_has_sss
+    # TODO:消したい
     self.albedo[active] = bsdf.eval_attribute(
         "single_scattering_albedo", si, bsdf_has_sss
     )
@@ -189,6 +154,7 @@ class SSSMedium:
       sample: mi.Float,
       channel: mi.UInt32,
       active: mi.Bool,
+      medium: mi.Spectrum,
       dwivedi_stretching: mi.Float = 1.0,
   ):
     """Samples a SSS interaction along the ray."""
@@ -199,17 +165,21 @@ class SSSMedium:
 
     # Dwivedi guiding implies a simple stretching of the sampled sigma_t
     # Careful to make a copy to get the channel value only!
+    
+    mei = medium.sample_interaction(ray, sample, channel, active)
     sampled_sigma_t = index_spectrum(
-        mi.Spectrum(self.sigma_t * dwivedi_stretching), channel
+        mi.Spectrum(mei.sigma_t * dwivedi_stretching), channel
     )
     sampled_t = -dr.log(1.0 - sample) / sampled_sigma_t
     valid_sss_its = active & (sampled_t <= dr.inf)
 
     sss_its.t = dr.select(valid_sss_its, sampled_t, dr.inf)
     sss_its.p = ray(sampled_t)
-    sss_its.sigma_t = self.sigma_t
-    sss_its.sigma_s = self.sigma_t * self.albedo
-    sss_its.g = self.g
+    sss_its.sigma_t = mei.sigma_t
+    sss_its.sigma_s = mei.sigma_s
+    phase = mei.medium.phase_function()
+    
+    sss_its.g = mei.g
 
     return sss_its
 
@@ -493,7 +463,11 @@ def sample_spectral_sss_scattering(
   # classic for the first distance sampling
   dwivedi_guided = mi.Bool(False)
   # TODO(pweier): Additionaly check for backward or forward guiding (for now, only forward)
-
+  
+  medium = dr.zeros(mi.MediumPtr)
+  medium = si.target_medium(ray.d)
+  phase_ctx = mi.PhaseFunctionContext(sampler)
+  phase = dr.zeros(mi.PhaseFunctionPtr)
   while dr.hint(
       active_medium,
       max_iterations=max_sss_depth,
@@ -510,6 +484,7 @@ def sample_spectral_sss_scattering(
         sampler.next_1d(active_medium),
         channel,
         active_medium,
+        medium,
         dr.select(dwivedi_guided, dwivedi_stretching, 1.0),
     )
 
@@ -668,309 +643,3 @@ def sample_spectral_sss_scattering(
     active_surface |= active_boundary_hit
 
   return sss_bounces, sss_distance, δg
-
-
-@dr.syntax
-def sample_spectral_sss_scattering_naive(
-    mode: dr.ADMode,
-    scene: mi.Scene,
-    sampler: mi.Sampler,
-    ray: mi.Ray3f,
-    si: mi.SurfaceInteraction3f,
-    sss_medium: SSSMedium,
-    max_sss_depth: int,
-    p_over_f,
-    active_surface: mi.Bool,
-    active_medium: mi.Bool,
-    eta: int,
-    sss_rr_depth: int,
-    channel: mi.UInt32,
-    medium_shape_bsdf: mi.BSDFPtr,
-    medium_shape_si: mi.SurfaceInteraction3f,
-    δL: mi.Spectrum,
-    L: mi.Spectrum,
-):
-  """Samples SSS scattering events.
-
-  Args:
-    mode: The AD mode.
-    scene: The Mitsuba scene.
-    sampler: The Mitsuba sampler.
-    ray: The ray to sample along.
-    sss_medium: The SSS medium.
-    max_sss_depth: The maximum SSS depth.
-    active_surface: Whether the surface is active.
-    active_medium: Whether the medium is active.
-    eta: The Russian roulette stopping probability.
-    sss_rr_depth: The depth at which to apply the Russian roulette stopping
-      probability.
-
-  Returns:
-    The volume throughput, the number of SSS bounces, and the total SSS
-    distance. (active_surface and active_medium are also updated)
-  """
-  primal = mode == dr.ADMode.Primal
-
-  sss_depth = mi.UInt32(0)
-
-  while dr.hint(
-      active_medium,
-      max_iterations=max_sss_depth,
-      label="SSS Loop (%s)" % mode.name,
-  ):
-    with dr.resume_grad(when=not primal):
-      sss_medium.populate(medium_shape_bsdf, medium_shape_si, active_medium)
-      sss_its = sss_medium.sample_interaction(
-          ray, sampler.next_1d(active_medium), channel, active_medium
-      )
-      sss_its.t = dr.detach(sss_its.t)
-
-      ray.maxt[active_medium & sss_its.is_valid()] = sss_its.t
-      # Compute a surface interaction that tracks derivatives arising
-      # from differentiable shape parameters (position, normals, etc.)
-      # In primal mode, this is just an ordinary ray tracing operation.
-      with dr.resume_grad(when=not primal):
-        si[active_medium] = scene.ray_intersect(ray, active_medium)
-      sss_its.t[active_medium & (si.t < sss_its.t)] = dr.inf
-
-      # Either a volume interaction was found or the next interaction is a
-      # surface
-      active_boundary_hit = active_medium & ~sss_its.is_valid()
-      active_medium &= sss_its.is_valid()
-      # Reset maxt for the boundary hit for the true next surface interaction
-      ray.maxt[active_boundary_hit] = dr.inf
-
-      # Evaluate ratio of transmittance and free-flight PDF
-      tr, free_flight_pdf = sss_medium.transmittance_eval_pdf(
-          sss_its, si, active_medium, active_boundary_hit
-      )
-      f = mi.Spectrum(1.0)
-      f[active_medium] *= sss_its.sigma_s
-      f[active_medium | active_boundary_hit] *= tr
-      update_weight_matrix(
-          p_over_f,
-          dr.detach(free_flight_pdf),
-          f,
-          active_medium | active_boundary_hit,
-      )
-      if dr.hint(not primal and dr.grad_enabled(f), mode="scalar"):
-        tr_pdf = index_spectrum(free_flight_pdf, channel)
-        weight = mi.Spectrum(1.0)
-        weight[active_medium] *= sss_its.sigma_s
-        weight[active_medium | active_boundary_hit] *= dr.select(
-            tr_pdf > 0.0, tr / dr.detach(tr_pdf), 0.0
-        )
-
-        # Differentiable version of the reflected radiance.
-        L_volume = dr.select(
-            active_medium | active_boundary_hit,
-            dr.detach(L / dr.maximum(1e-8, weight)),
-            0.0,
-        )
-
-        # Propagate derivatives from/to 'Lo' based on 'mode'
-        if mode == dr.ADMode.Backward:
-          dr.backward_from(δL * L_volume * weight)
-        else:
-          δL += dr.forward_to(L_volume * weight)
-
-      # Sample isotropic phase function
-      with dr.suspend_grad():
-        phase_wo, phase_weight, phase_pdf = sample_spectral_hg(
-            sss_its.g, ray, sampler.next_2d(active_medium), channel
-        )
-      update_weight_matrix(
-          p_over_f,
-          phase_pdf,
-          phase_weight * phase_pdf,
-          active_medium,
-      )
-
-      # Re evaluate the phase function value in an attached manner
-      phase_eval = eval_spectral_hg(sss_its.g, phase_wo, ray)
-      if dr.hint(not primal and dr.grad_enabled(phase_eval), mode="scalar"):
-        # Differentiable version of the reflected radiance.
-        L_volume = phase_eval * dr.select(
-            active_medium,
-            dr.detach(L / dr.maximum(1e-8, phase_eval)),
-            0.0,
-        )
-
-        # Propagate derivatives from/to 'Lo' based on 'mode'
-        if mode == dr.ADMode.Backward:
-          dr.backward_from(δL * L_volume)
-        else:
-          δL += dr.forward_to(L_volume)
-
-    # Construct next ray
-    ray[active_medium] = mi.Ray3f(sss_its.p, phase_wo)
-    sss_depth[active_medium] += 1
-
-    # -------------------- Stopping criterion ---------------------
-
-    volume_throughput_mis = mis_weight_from_matrix(p_over_f)
-    # Don't run another iteration if the throughput has reached zero
-    volume_throughput_max = dr.max(volume_throughput_mis)
-    active_medium &= volume_throughput_max != 0
-    active_boundary_hit &= volume_throughput_max != 0
-
-    # Russian roulette stopping probability (must cancel out ior^2
-    # to obtain unitless throughput) no min probability for volume scattering
-    rr_prob = dr.minimum(volume_throughput_max * eta**2, 1.0)
-
-    # Apply only further along the path since, this introduces variance
-    rr_active = sss_depth >= sss_rr_depth
-    update_weight_matrix(
-        p_over_f, dr.detach(rr_prob), mi.Spectrum(1.0), rr_active
-    )
-    rr_continue = sampler.next_1d() < rr_prob
-    active_medium &= ~rr_active | rr_continue
-    # sss_depth < max_sss_depth + 1 ensures that an sss_depth of 1 will
-    # effectively trace TWO rays in the volume one for finding the first medium
-    # interaction and one to hit the boundary.
-    active_medium &= sss_depth < max_sss_depth + 1
-
-    active_surface |= active_boundary_hit
-
-  return
-
-
-def albedo_inversion_isotropic(
-    surface_colour: np.ndarray,
-    scattering_radius: np.ndarray,
-):
-  """Computes the scattering albedo and extinction from surface albedo.
-
-  Args:
-    surface_colour: The surface colour of the object.
-    scattering_radius: The scattering radius.
-    anisotropy: The anisotropy of the phase function
-
-  Returns:
-    The scattering albedo and extinction coefficients.
-  """
-
-  a = surface_colour
-  d = scattering_radius
-
-  # -5.09406f * A + 2.61188f * A * A - 4.31805f * A * A * A)
-  scattering_albedo = 1.0 - np.exp(
-      -5.09406 * a + 2.61188 * a * a - 4.31805 * a * a * a
-  )
-  # scattering_albedo = 1.0 - np.exp(-3.25172*a*a*a + 1.2166*a*a - 4.75914*a)
-  s = 1.9 - a + 3.5 * (a - 0.8) * (a - 0.8)
-
-  # this trick is used in the cycles reference implementation
-  scattering_albedo = np.clip(scattering_albedo, 0.0, 0.999999)
-
-  # following cycle's implementation
-  sigma_t = 1.0 / (d * s)
-
-  # there is additional clamping and rescaling going on for low albedo values
-  return scattering_albedo, sigma_t
-
-
-def albedo_inversion(
-    surface_colour: np.ndarray,
-    scattering_radius: np.ndarray,
-    anisotropy: np.ndarray,
-):
-  """Computes the scattering albedo and extinction from surface albedo.
-
-  Args:
-    surface_colour: The surface colour of the object.
-    scattering_radius: The scattering radius.
-    anisotropy: The anisotropy of the phase function
-
-  Returns:
-    The scattering albedo and extinction coefficients.
-  """
-
-  g = anisotropy
-  g2 = np.power(anisotropy, 2)
-  g3 = np.power(anisotropy, 3)
-  g4 = np.power(anisotropy, 4)
-  g5 = np.power(anisotropy, 5)
-  g6 = np.power(anisotropy, 6)
-  g7 = np.power(anisotropy, 7)
-
-  # first, we compute the coefficients
-  # can be vastly simplified for the isotropic case
-  A = (
-      1.8260523782
-      + -1.28451056436 * g
-      + -1.79904629312 * g2
-      + 9.193932892020 * g3
-      + -22.8215585862 * g4
-      + 32.02348742590 * g5
-      + -23.6264803333 * g6
-      + 7.210670026580 * g7
-  )
-
-  B = 4.98511194385 + 0.127355959438 * np.exp(
-      31.1491581433 * g
-      + -201.847017512 * g2
-      + 841.5760167230 * g3
-      + -2018.09288505 * g4
-      + 2731.715602860 * g5
-      + -1935.41424244 * g6
-      + 559.0090544740 * g7
-  )
-
-  C = (
-      1.09686102424
-      + -0.394704063468 * g
-      + 1.0525811594100 * g2
-      + -8.839637127260 * g3
-      + 28.864323066100 * g4
-      + -46.88029135810 * g5
-      + 38.540283751800 * g6
-      + -12.71810425380 * g7
-  )
-
-  D = (
-      0.496310210422
-      + 0.360146581622 * g
-      + -2.15139309747 * g2
-      + 17.88968992170 * g3
-      + -55.2984010333 * g4
-      + 82.06598224300 * g5
-      + -58.5106008578 * g6
-      + 15.84782950210 * g7
-  )
-
-  E = 4.23190299701 + 0.00310603949088 * np.exp(
-      76.7316253952 * g
-      + -594.356773233 * g2
-      + 2448.883420300 * g3
-      + -5576.68528998 * g4
-      + 7116.601719120 * g5
-      + -4763.54467887 * g6
-      + 1303.531805500 * g7
-  )
-
-  F = (
-      2.40602999408
-      + -2.51814844609 * g
-      + 9.184949083560 * g2
-      + -79.2191708682 * g3
-      + 259.0828682090 * g4
-      + -403.613804597 * g5
-      + 302.8571243600 * g6
-      + -87.4370473567 * g7
-  )
-
-  linear_weight = np.power(surface_colour, 0.25)
-
-  scattering_albedo = (1.0 - linear_weight) * A * np.power(
-      np.arctan(B * surface_colour), C
-  ) + linear_weight * D * np.power(np.arctan(E * surface_colour), F)
-
-  # this trick is used in the cycles reference implementation
-  scattering_albedo = np.clip(scattering_albedo, 0.0, 0.999999)
-
-  # following cycle's implementation
-  sigma_t = (1.0 / np.maximum(scattering_radius, 1e-16)) / (1.0 - g)
-
-  # there is additional clamping and rescaling going on for low albedo values
-  return scattering_albedo, sigma_t
